2ae4de5b66b8cc2e39b3f09010b74227738372f3
lucas rodrigues <rodrigueslucas062@gmail.com>
2023-04-03 00:02:21 -0300

    Update Gerador.py

--- Gerador.py
+++ Gerador.py
@@ -2,13 +2,15 @@ import random
 
 letra_maiuscula = chr(random.randint(65, 90))
 letra_minuscula = chr(random.randint(90, 122))
-char_especiais = chr(33)
+char_especiais = chr(64)
 lista_numeros = []
 
-for i in range(8):
+for i in range(15):
     numeros = random.randrange(9)
     lista_numeros.append(numeros)
 
 random.shuffle(lista_numeros)
 lista_numeros = str(lista_numeros) [1: -1]
-lista_numeros = lista_numeros.replace
+lista_numeros = lista_numeros.replace(',', '')
+
+print (letra_maiuscula, letra_minuscula, char_especiais, lista_numeros)
